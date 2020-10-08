40b550f0ef5e7624611993f6f4a28a371dcecc35
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-10-07 20:44:28 -0700

    Created a basic script to print on my home printer

---
+++ print-to-printer.py
@@ -0,0 +1,15 @@
+import sockets
+
+PRINTER_IP_ADDRESS = '192.168.1.87'
+PORT = 9100
+
+printer = socket.socket()
+printer.connect((PRINTER_IP_ADDRESS, PORT))
+# NOTE: This is an unreliable way to print.
+# This method of sending data to the printer
+# also cause weird printing output.
+# You need to do more research on the exact
+# way the data should be formatted to print
+# as we would normally expect text to be printed.
+printer.send(b'Please print this text')
+printer.close()
