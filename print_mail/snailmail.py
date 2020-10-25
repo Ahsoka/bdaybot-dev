a388f38abd42dfb9de3343dd01f966447572f636
ryanl <ryanlee619@gmail.com>
2020-10-24 17:36:24 -0700

    Created file to make envelope pdf gmae epic wow cool

---
+++ print_mail/snailmail.py
@@ -0,0 +1,43 @@
+#envelope size: 110 by 145 mm
+
+# Elliot Torres
+# 4321 Loser Road
+# La Crescenta, CA 91214
+#
+# Ryan Lee
+# 1234 Boomer Road
+# La Crescenta, CA 91214
+
+import os
+from dotenv import load_dotenv, find_dotenv
+from fpdf import FPDF
+import pathlib
+
+load_dotenv(find_dotenv())
+
+sender_name = os.environ['sender_name']
+sender_addr1 = os.environ['sender_addr1']
+sender_addr2 = os.environ['sender_addr2']
+
+recipient_name = os.environ['recipient_name']
+recipient_addr1 = os.environ['recipient_addr1']
+recipient_addr2 = os.environ['recipient_addr2']
+
+pdf = FPDF('L', 'mm', (110, 145))
+pdf.add_page()
+pdf.set_font('Times', '', 9.8)
+pdf.set_margins(0, 0, 0)
+
+pdf.text(7, 7.5, sender_name)
+pdf.text(7, 10.5, sender_addr1)
+pdf.text(7, 13.5, sender_addr2)
+
+pdf.set_font('Times', '', 14)
+pdf.text(44, 78, recipient_name)
+pdf.text(44, 82, recipient_addr1)
+pdf.text(44, 86, recipient_addr2)
+
+envelope_file = pathlib.Path('envelope.pdf')
+if envelope_file.exists():
+    envelope_file.unlink()
+pdf.output('envelope.pdf', dest='F').encode('latin-1')
