f88e9e51a8a997660329aad4185995b4939166ef
Elliot <36275109+Falcons-Royale@users.noreply.github.com>
2021-01-17 18:23:53 -0800

    made it into method

--- print_mail/snailmail.py
+++ print_mail/snailmail.py
@@ -17,39 +17,40 @@ load_dotenv(find_dotenv())
 
 # types out address on envelope
 
-sender_name = os.environ['sender_name']
-sender_addr1 = os.environ['sender_addr1']
-sender_addr2 = os.environ['sender_addr2']
+def sendmail(FULLNAME,
+             ADDRESS_LINE_ONE,
+             CITY,
+             STATE,
+             ZIPCODE):
+    sender_name = os.environ['sender_name']
+    sender_addr1 = os.environ['sender_addr1']
+    sender_addr2 = os.environ['sender_addr2']
 
-recipient_name = os.environ['recipient_name']
-recipient_addr1 = os.environ['recipient_addr1']
-recipient_addr2 = os.environ['recipient_addr2']
+    pdf = FPDF('L', 'mm', (110, 145))
+    pdf.add_page()
+    pdf.set_font('Times', '', 9.8)
+    pdf.set_margins(0, 0, 0)
 
-pdf = FPDF('L', 'mm', (110, 145))
-pdf.add_page()
-pdf.set_font('Times', '', 9.8)
-pdf.set_margins(0, 0, 0)
+    pdf.text(7, 7.5, sender_name)
+    pdf.text(7, 10.5, sender_addr1)
+    pdf.text(7, 13.5, sender_addr2)
 
-pdf.text(7, 7.5, sender_name)
-pdf.text(7, 10.5, sender_addr1)
-pdf.text(7, 13.5, sender_addr2)
+    pdf.set_font('Times', '', 14)
+    pdf.text(44, 78, FULLNAME)
+    pdf.text(44, 82, ADDRESS_LINE_ONE)
+    pdf.text(44, 86, f'{CITY}, {STATE} {ZIPCODE}')
 
-pdf.set_font('Times', '', 14)
-pdf.text(44, 78, recipient_name)
-pdf.text(44, 82, recipient_addr1)
-pdf.text(44, 86, recipient_addr2)
+    # types out message on back fo envelope
 
-# types out message on back fo envelope
+    pdf.add_page()
+    pdf.set_margins(0, 0, 0)
 
-pdf.add_page()
-pdf.set_margins(0, 0, 0)
+    pdf.text(10, 78, f"Happy Birthday {recipient_name}!")
+    pdf.text(10, 82, "Have a wonderful day and enjoy your sweet!")
+    pdf.text(10, 86, "-CVHS Bday Team")
 
-pdf.text(10, 78, f"Happy Birthday {recipient_name}!")
-pdf.text(10, 82, "Have a wonderful day and enjoy your sweet!")
-pdf.text(10, 86, "-CVHS Bday Team")
-
-envelope_file = pathlib.Path('envelope.pdf')
-if envelope_file.exists():
-    envelope_file.unlink()
-pdf.output('envelope.pdf', dest='F').encode('latin-1')
-os.system("lp -d printer envelope.pdf")
+    envelope_file = pathlib.Path('envelope.pdf')
+    if envelope_file.exists():
+        envelope_file.unlink()
+    pdf.output('envelope.pdf', dest='F').encode('latin-1')
+    os.system("lp -d printer envelope.pdf")
